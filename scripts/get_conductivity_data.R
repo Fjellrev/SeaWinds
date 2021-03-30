7ffc48a447b768318fa9c21bf3a7ce46bdb7845a
Simon Lacombe <simon.lacombe@ens-lyon.fr>
2021-03-30 15:31:00 +0200

    Get only the midnight position and get mean conductivity between t and t+1

--- scripts/get_conductivity_data.R
+++ scripts/get_conductivity_data.R
@@ -10,11 +10,15 @@ path_act = "data/activity"
 path_bird = "data/Kittiwake_data"
   
 ###GET BIRD DATA ----
+
 birdRDS <- list.files(path=path_bird, pattern = "5col")
 bird_data <- readRDS(paste0(path_bird,"/", birdRDS)) %>% as.data.table
 bird_data[, timestamp := as.POSIXct(bird_data$timestamp)]
-bird_data = bird_data[abs(as.numeric(format(bird_data$timestamp, "%H"))-12)>6] #keep only the midnight positions
+bird_data <- bird_data[not(abs(as.numeric(format(bird_data$timestamp, "%H"))-12)<6)] #keep only the midnight positions
+
+
 ###ADD THE ACTIVITY DATA ----
+
 bird_act <- data.table(ring=c(),timestamp=c(),std_cond=c())
 
 ring <- unique(bird_data$ring)
@@ -22,15 +26,20 @@ for (id in ring)
 {
 cat(id)
 actRDS <- list.files(path=path_act, pattern = id)
-act_data <- readRDS(paste0(path_act,"/",actRDS)) %>% as.data.table
-act_data[, timestamp := as.POSIXct(act_data$timestamp)]
-id_timestamp <- as.POSIXct(bird_data$timestamp[bird_data$ring==id])
-mean_act <- c()
-for (t in format(id_timestamp, "%D"))
+act_data <- readRDS(paste0(path_act,"/",actRDS)) %>% as.data.table #activity for the individual id
+act_data[, timestamp := as.POSIXct(act_data$timestamp)] 
+id_timestamp <- as.POSIXct(bird_data$timestamp[bird_data$ring==id]) #list of the GLS timestamp for the individual id
+id_act <- c() 
+
+for (i in 1:(length(id_timestamp)-1)) #for each timestamp get the conductivity between t and t+1
 {
-mean_act <- append(mean_act, mean(act_data$std_conductivity[format(act_data$timestamp, "%D") ==t], na.rm = T))
+t <- id_timestamp[i]
+t_dt <- id_timestamp[i+1]
+act <- mean(act_data$std_conductivity[act_data$timestamp>t&act_data$timestamp<t_dt], na.rm = T)#mean activity between t and t+1 
+id_act <- append(id_act, act)
 }
-bird_act <- rbind(bird_act, data.table(ring=rep(id, length(id_timestamp)),timestamp=id_timestamp, std_cond=mean_act))
+
+bird_act <- rbind(bird_act, data.table(ring=rep(id, length(id_timestamp)),timestamp=id_timestamp, std_cond=append(id_act,NA)))
 }
 
 bird_data[,std_conductivity := bird_act$std_cond]
