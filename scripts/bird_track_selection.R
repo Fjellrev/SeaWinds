d8dfd85fd4cca21a599a0123cac93561b517d8de
Simon Lacombe <simon.lacombe@ens-lyon.fr>
2021-05-07 13:02:20 +0200

    distinct between spring and autumn

--- scripts/bird_track_selection.R
+++ scripts/bird_track_selection.R
@@ -13,25 +13,30 @@ path_bird <- "data/Kittiwake_data_treated"
 path_wind <- "data/ERA-Interrim"
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
-colonies <- c("Isle of May", "Alkefjellet") #studied colonies
+colonies_out <- c() #studied colonies
 min_length <- 2e06
 min_npos <- 5
 
 ###GET DATA ----
 
-birdRDS <- list.files(path=path_bird, pattern = "5col")
+birdRDS <- list.files(path=path_bird, pattern = "BLKI_5col_2016")
 bird_data <- readRDS(paste0(path_bird,'/', birdRDS)) %>% as.data.table
 bird_data[, timestamp := as.POSIXct(bird_data$timestamp)] 
 setnames(bird_data, c("lon","lat"),c("x", "y"))
-bird_data_ <- bird_data[bird_data$colony%in%colonies]
+
+bird_data_ <- bird_data[not(bird_data$colony%in%colonies_out)]
 bird_data_ <- bird_data_[bird_data_$migr10==1]
+
+bird_data_[, migr_type := "spr"]
+bird_data_$migr_type[as.numeric(format(bird_data_$timestamp, "%m"))>6] <- "aut"
+bird_data_[, journey := paste0(ring, "-",  migr_type)]
 
 ###PREPARE DATASET ----
 
 rm <- c() #tracks to be removed
-for (id in unique(bird_data_$ring))
+for (id in unique(bird_data_$journey))
 {
-  track_id <- bird_data_[bird_data_$ring==id]
+  track_id <- bird_data_[bird_data_$journey==id]
   npos <- nrow(track_id)
   length_track_id <- distGeo(c(track_id$x[1],track_id$y[1]),c(track_id$x[npos],track_id$y[npos]))
   if (npos<min_npos|length_track_id<min_length)
@@ -40,6 +45,7 @@ for (id in unique(bird_data_$ring))
   }
 }
 
-bird_data_ <- bird_data_[not(bird_data_$ring%in%rm)]
+bird_data_ <- bird_data_[not(bird_data_$journey%in%rm)]
 
-saveRDS(bird_data_,"data/Kittiwake_data_treated/BLKI_Alkefjellet_May_tracks.rds")
+saveRDS(bird_data_[bird_data_$migr_type == "spr"],"data/Kittiwake_data_treated/BLKI_tracks_spring.rds")
+saveRDS(bird_data_[bird_data_$migr_type == "aut"],"data/Kittiwake_data_treated/BLKI_tracks_autumn.rds")
