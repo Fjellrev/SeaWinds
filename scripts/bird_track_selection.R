1f21bf0138f126053df27d9483c44bf13c71fd0b
Simon Lacombe <simon.lacombe@ens-lyon.fr>
2021-05-07 17:57:35 +0200

    identifies bursts

--- scripts/bird_track_selection.R
+++ scripts/bird_track_selection.R
@@ -14,7 +14,7 @@ path_wind <- "data/ERA-Interrim"
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
 colonies_out <- c() #studied colonies
-min_length <- 2e06
+min_length <- 1e06
 min_npos <- 5
 
 ###GET DATA ----
@@ -24,19 +24,21 @@ bird_data <- readRDS(paste0(path_bird,'/', birdRDS)) %>% as.data.table
 bird_data[, timestamp := as.POSIXct(bird_data$timestamp)] 
 setnames(bird_data, c("lon","lat"),c("x", "y"))
 
+bird_data[, burst := rleid(bird_data$migr10, bird_data$ring)]
+
 bird_data_ <- bird_data[not(bird_data$colony%in%colonies_out)]
 bird_data_ <- bird_data_[bird_data_$migr10==1]
 
-bird_data_[, migr_type := "spr"]
-bird_data_$migr_type[as.numeric(format(bird_data_$timestamp, "%m"))>6] <- "aut"
-bird_data_[, journey := paste0(ring, "-",  migr_type)]
+bird_data_[, migr_month := mean(bird_data_$timestamp[bird_data_$burst==burst]), by = 1:nrow(bird_data_)]
+bird_data_$migr_month <- format(bird_data_$migr_month,"%m")
+bird_data_$burst <- paste0(bird_data_$ring,"-", bird_data_$burst)
 
 ###PREPARE DATASET ----
 
 rm <- c() #tracks to be removed
-for (id in unique(bird_data_$journey))
+for (id in unique(bird_data_$burst))
 {
-  track_id <- bird_data_[bird_data_$journey==id]
+  track_id <- bird_data_[bird_data_$burst==id]
   npos <- nrow(track_id)
   length_track_id <- distGeo(c(track_id$x[1],track_id$y[1]),c(track_id$x[npos],track_id$y[npos]))
   if (npos<min_npos|length_track_id<min_length)
@@ -45,7 +47,6 @@ for (id in unique(bird_data_$journey))
   }
 }
 
-bird_data_ <- bird_data_[not(bird_data_$journey%in%rm)]
+bird_data_ <- bird_data_[not(bird_data_$burst%in%rm)]
 
-saveRDS(bird_data_[bird_data_$migr_type == "spr"],"data/Kittiwake_data_treated/BLKI_tracks_spring.rds")
-saveRDS(bird_data_[bird_data_$migr_type == "aut"],"data/Kittiwake_data_treated/BLKI_tracks_autumn.rds")
+saveRDS(bird_data_,"data/Kittiwake_data_treated/BLKI_tracks.rds")
